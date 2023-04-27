72f88529905c685db28cf235c60d2aba045b81a3
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-04-27 18:39:29 +0200

    commit generale, non riuscivo a installare node ma dovrebbe andare

---
+++ hello-world/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+
+import appTitle from './components/AppTitle.vue'
+
+  export default{
+    name: 'App',
+    components:{
+      appTitle
+    }
+  }
+</script>
+
+<template>
+
+  <appTitle />
+
+</template>
+
+<style>
+
+</style>
